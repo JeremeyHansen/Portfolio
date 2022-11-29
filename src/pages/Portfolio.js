b393cb1ce1aca69e74d2637337de7d9cefaf891a
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-11-29 12:54:17 -0500

    build issue

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,6 @@
 import Flatify from "../Flatiify.mp4";
-import OnlyJams from "../onlyjamsimg.jpg";
-import Noteful from "../notefulimg.jpg";
+import OnlyJams from "../OnlyJams.mp4";
+import Noteful from "../Noteful.mp4";
 import "../css/portfolio.css";
 import OnlyJamsPopup from "./OnlyJams.js";
 import FlatifyPopup from "./Flatify.js";
@@ -9,8 +9,7 @@ import NotefulPopup from "./Noteful.js";
 import { useState } from "react";
 import { GoMarkGithub } from "react-icons/go";
 import Brewery from "../brewery.jpg";
-import WeSearch from "../wesearchimg.jpg";
-
+import WeSearch from "../wesearch.mp4";
 
 export default function Portfolio() {
   const [openFlatify, setOpenFlatify] = useState(false);
@@ -22,7 +21,6 @@ export default function Portfolio() {
   const [notefulTech, setNotefulTech] = useState(false);
   const [weSearchTech, setWeSearcTech] = useState(false);
   const [whatsBrewinTech, setWhatsBrewinTech] = useState(false);
-
 
   const handleOpenFlatify = () => {
     setOpenFlatify(true);
@@ -47,11 +45,12 @@ export default function Portfolio() {
       </div>
       <div className="card-container">
         <div className="portfolio-cards">
-          <img
+          <video
             src={OnlyJams}
             alt="Only Jams"
+            muted
             onClick={handleOpenOnlyJams}
-          ></img>
+          ></video>
           <h2>
             OnlyJams
             <a
@@ -61,8 +60,10 @@ export default function Portfolio() {
               <GoMarkGithub />
             </a>
           </h2>
-          <p onClick={() => setOnlyJamsTech(!onlyJamsTech)} >
-          {onlyJamsTech ? "Technologies Used: React, Ruby, Rails, PostgreSql, CSS" : `As a musician, I've realized the social media realm lacks a platform
+          <p onClick={() => setOnlyJamsTech(!onlyJamsTech)}>
+            {onlyJamsTech
+              ? "Technologies Used: React, Ruby, Rails, PostgreSql, CSS"
+              : `As a musician, I've realized the social media realm lacks a platform
             for users to share content solely based on music. OnlyJams is here
             to that solve problem by offering an application for users to send
             the music they love to their friends and the world. Key features:
@@ -71,14 +72,14 @@ export default function Portfolio() {
             messenger system.`}
           </p>
           <p>Click image for video demo</p>
-
         </div>
         <div className="portfolio-cards">
-          <img
+          <video
+            muted
             src={Noteful}
-         alt="Noteful"
+            alt="Noteful"
             onClick={handleOpenNoteful}
-          ></img>
+          ></video>
           <h2>
             Noteful
             <a
@@ -89,7 +90,9 @@ export default function Portfolio() {
             </a>
           </h2>
           <p onClick={() => setNotefulTech(!notefulTech)}>
-          {notefulTech ? "Technologies Used: React, Ruby, Rails, PostgreSql, CSS" : `Noteful was designed to help solve the problem of missing important
+            {notefulTech
+              ? "Technologies Used: React, Ruby, Rails, PostgreSql, CSS"
+              : `Noteful was designed to help solve the problem of missing important
             information. Noteful grants users the ability to create and
             subscribe to groups where they can view notes from classmates,
             co-workers/colleagues or whomever else is sharing relative
@@ -99,7 +102,6 @@ export default function Portfolio() {
             delete notes.`}
           </p>
           <p className="video-demo">Click image for video demo</p>
-
         </div>
         <div className="portfolio-cards">
           <video
@@ -107,32 +109,35 @@ export default function Portfolio() {
             alt="Flatify"
             muted
             onClick={handleOpenFlatify}
-            ></video>
+          ></video>
           <h2>
             Flatify
             <a
               className="git-logo"
               href="https://github.com/JeremeyHansen/flatify2"
-              >
+            >
               <GoMarkGithub />
             </a>
           </h2>
           <p onClick={() => setFlatifyTech(!flatifyTech)}>
-            { flatifyTech ? "Technologies Used: React, Spotify's API, REST Design, CSS" : `A Spotify-inspired music application created to simplify the process
+            {flatifyTech
+              ? "Technologies Used: React, Spotify's API, REST Design, CSS"
+              : `A Spotify-inspired music application created to simplify the process
             of finding music and creating playlists. Taking advantage of
             Spotify's API, we built a fully functional music streaming
             application to search music and create and edit playlists that will
             persist to your Spotify account. Flatify also has a built in audio
             player to play whatever music you desire.`}
           </p>
-            <p className="video-demo">Click image for video demo</p>
+          <p className="video-demo">Click image for video demo</p>
         </div>
         <div className="portfolio-cards">
-          <img
-          src={WeSearch}
-alt="We Search."
-          onClick={handleOpenWeSearch}
-          ></img>
+          <video
+            muted
+            src={WeSearch}
+            alt="We Search."
+            onClick={handleOpenWeSearch}
+          ></video>
           <h2>
             We Search.
             <a
@@ -143,13 +148,14 @@ alt="We Search."
             </a>
           </h2>
           <p onClick={() => setWeSearcTech(!weSearchTech)}>
-            {(weSearchTech) ? "Technologies Used: React, Ruby, SQLite3, CSS": `We Search is an Atlanta based Yelp mock up. Users can sign up and
+            {weSearchTech
+              ? "Technologies Used: React, Ruby, SQLite3, CSS"
+              : `We Search is an Atlanta based Yelp mock up. Users can sign up and
             add whatever places they like as well as leave reviews on previously
             listed places. Key features: backend REST API, JWT user
             authentication, full CRUD on two models.`}
           </p>
           <p className="video-demo">Click image for video demo</p>
-
         </div>
         <div className="portfolio-cards">
           <img src={Brewery} alt="What's Brewin'?"></img>
@@ -163,7 +169,9 @@ alt="We Search."
             </a>
           </h2>
           <p onClick={() => setWhatsBrewinTech(!whatsBrewinTech)}>
-            {whatsBrewinTech ? "Technologies used: Vanilla Javascript, HTML, CSS, Public API" : `What's Brewin'? is a single page application to search and find
+            {whatsBrewinTech
+              ? "Technologies used: Vanilla Javascript, HTML, CSS, Public API"
+              : `What's Brewin'? is a single page application to search and find
             local breweries in your area. A user can input what city they're
             located in to get a list of breweries in their area. They also are
             granted the ability to comment on the breweries as well as delete
